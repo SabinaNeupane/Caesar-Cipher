5bbd1ad2b218bb62734a3b93361e12b4090d38ac
SabinaNeupane <sabina.neuwork@gmail.com>
2025-12-28 15:00:35 +0545

    coded basic logic

--- cipher.py
+++ cipher.py
@@ -1,1 +1,10 @@
 letters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+msg=input("enter your string : ")
+n_msg=""
+for i in msg.upper():
+    if i != " ":
+        idx=letters.index(i)
+        n_msg=n_msg+(letters[idx+3])
+    else:
+        n_msg=n_msg+" "
+print(n_msg)
