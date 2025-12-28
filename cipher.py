a6d88c4f1b956ac886e881d7e7ef4181d13377a0
SabinaNeupane <sabina.neuwork@gmail.com>
2025-12-28 16:34:49 +0545

    made logical changes to wrap Z to A if needed

--- cipher.py
+++ cipher.py
@@ -6,17 +6,22 @@ while True:
     1. Encrypt
     2. Exit ''')
     opt=int(input("enter your choice : "))
-    def input_shell(msg):
+    def input_shell(msg,sv):
                 for i in msg.upper():
                     global n_msg
                     if i != " ":
                         idx=letters.index(i)
-                        n_msg=n_msg+(letters[idx+3])
+                        if (idx+sv)>(len(letters)-1):
+                            o1=letters.index("Z")-idx
+                            o2=sv-o1
+                            n_msg=n_msg+(letters[o2-1])
+                        else:
+                             n_msg=n_msg+(letters[idx+sv])
                     else:
                         n_msg=n_msg+" "
                 return n_msg        
     if opt==1:
-        ec=input_shell(input("enter your string : "))
+        ec=input_shell(input("enter your string : "),int(input("enter your shift value : ")))
         print()
         print(f"the message is {ec}")
         print()
