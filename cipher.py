04ec78a7ea87eb0b8be6d0191d941c1197528a8b
SabinaNeupane <sabina.neuwork@gmail.com>
2025-12-28 16:00:07 +0545

    converted logic into structured app-like code

--- cipher.py
+++ cipher.py
@@ -1,10 +1,26 @@
 letters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-msg=input("enter your string : ")
-n_msg=""
-for i in msg.upper():
-    if i != " ":
-        idx=letters.index(i)
-        n_msg=n_msg+(letters[idx+3])
-    else:
-        n_msg=n_msg+" "
-print(n_msg)
+while True:
+    n_msg=""
+    print("Welcome to Caesar Cipher")
+    print('''Pick from the following options : 
+    1. Encrypt
+    2. Exit ''')
+    opt=int(input("enter your choice : "))
+    def input_shell(msg):
+                for i in msg.upper():
+                    global n_msg
+                    if i != " ":
+                        idx=letters.index(i)
+                        n_msg=n_msg+(letters[idx+3])
+                    else:
+                        n_msg=n_msg+" "
+                return n_msg        
+    if opt==1:
+        ec=input_shell(input("enter your string : "))
+        print()
+        print(f"the message is {ec}")
+        print()
+    if opt==2:
+        print("you have exited the program")
+        break
+
